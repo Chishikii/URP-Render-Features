2b66f1d6234b120bac6cb287a5309658643b698b
Chishikii <finn@pelzers.com>
2024-10-19 17:24:49 +0200

    implemented new render graph api

--- Assets/Render Features/Outlines/OutlineRenderFeature.cs
+++ Assets/Render Features/Outlines/OutlineRenderFeature.cs
@@ -1,88 +1,66 @@
-﻿using System;
+using System;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering.RenderGraphModule;
 
-namespace RenderFeatures.Outlines
+namespace Xenon
 {
-    [Serializable]
-    public class OutlineSettings
-    {
-        /// <summary>
-        /// When to render the outlines.
-        /// </summary>
-        public RenderPassEvent RenderPassEvent = RenderPassEvent.BeforeRenderingTransparents;
-
-        /// <summary>
-        /// Format of the render texture. In order to enable HDR dependent post processing use at least ARGBHalf.
-        /// </summary>
-        public RenderTextureFormat RenderTextureFormat = RenderTextureFormat.Default;
-
-        /// <summary>
-        /// The amount of bits restored for the depth buffer.
-        /// </summary>
-        public DepthBits DepthBufferBits = DepthBits.Depth8;
-
-        /// <summary>
-        /// Color used to fill empty areas.
-        /// </summary>
-        public Color BackgroundColor = Color.black;
-
-        /// <summary>
-        /// The layer mask of the objects to include in the outlines.
-        /// </summary>
-        public LayerMask LayerMask = 0;
-
-        /// <summary>
-        /// The render layer mask of the objects to include in the outlines.
-        /// </summary>
-        public int RenderLayerMask = 1;
-
-        public float OutlineScale = 1;
-        public Color OutlineColor = new(0, 22, 255);
-        public float RobertsCrossMultiplier = 500;
-
-        public float DepthThreshold = 10;
-        public float NormalThreshold = 0.6f;
-
-        public float SteepAngleThreshold = 0;
-        public float SteepAngleMultiplier = 100;
-    }
-
     public class OutlineRenderFeature : ScriptableRendererFeature
     {
-        /// <summary>
-        /// The settings used for the outline renderer feature.
-        /// </summary>
-        public OutlineSettings Settings = new();
+        [Serializable]
+        public class Settings
+        {
+            public RenderPassEvent RenderPassEvent = RenderPassEvent.AfterRenderingTransparents;
 
-        private OutlineRenderPass m_OutlineRenderPass;
+            public LayerMask LayerMask = 0;
 
-        /// <summary>
-        /// Initializes this feature's resources. This is called every time serialization happens.
-        /// </summary>
+            public RenderingLayerMask RenderingLayerMask = 0;
+
+            public Material OverrideMaterial;
+
+            public Material BlitMaterial;
+
+            public bool ClearDepth;
+        }
+
+        [Serializable]
+        public class OutlineSettings
+        {
+            public float OutlineScale = 1f;
+            public float RobertsCrossMultiplier = 100;
+            public float DepthThreshold = 10f;
+            public float NormalThreshold = 0.4f;
+            public float SteepAngleThreshold = 0.2f;
+            public float SteepAngleMultiplier = 25f;
+            public Color OutlineColor = Color.white;
+        }
+
+        public class OutlineData : ContextItem
+        {
+            public TextureHandle FilterTextureHandle;
+
+            public override void Reset()
+            {
+                FilterTextureHandle = TextureHandle.nullHandle;
+            }
+        }
+
+        public Settings FeatureSettings;
+        public OutlineSettings MaterialSettings;
+
+        private OutlinePassFilter _outlinePassFilter;
+        private OutlinePassFinal _outlinePassFinal;
+
         public override void Create()
         {
-            m_OutlineRenderPass = new OutlineRenderPass(Settings);
+            _outlinePassFilter = new OutlinePassFilter(FeatureSettings);
+            _outlinePassFinal = new OutlinePassFinal(FeatureSettings, MaterialSettings);
         }
-
-        /// <summary>
-        /// Injects one or multiple <c>ScriptableRenderPass</c> in the renderer.
-        /// </summary>
-        /// <param name="renderer">Renderer used for adding render passes.</param>
-        /// <param name="renderingData">Rendering state. Use this to setup render passes.</param>
         public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
         {
-            renderer.EnqueuePass(m_OutlineRenderPass);
-        }
-
-        /// <summary>
-        /// Clean up any resources used by the render feature and pass.
-        /// </summary>
-        protected override void Dispose(bool disposing)
-        {
-            // Make sure we also clean up the passes resources.
-            m_OutlineRenderPass.Dispose();
+            renderer.EnqueuePass(_outlinePassFilter);
+            renderer.EnqueuePass(_outlinePassFinal);
         }
     }
 }
